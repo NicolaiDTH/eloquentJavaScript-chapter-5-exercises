9824700bc412a8b759bd37e86ece2bb39706b5ed
Nicolai <nicolai@mmg.io>
2017-02-23 16:48:31 +0000

    Higher-Order Functions Exercises

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+console.log(average(differences));
+// → 31.2
